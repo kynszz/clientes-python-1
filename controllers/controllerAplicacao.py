25c38a0b1fcb12d49b436fa23a1fec4107d90e1b
Kynszz <106875528+kynszz@users.noreply.github.com>
2022-07-23 13:34:43 -0300

    att

--- controllers/controllerAplicacao.py
+++ controllers/controllerAplicacao.py
@@ -7,6 +7,7 @@ def validar_login(usuario_completo):
     senha_BD = banco.model_senha()
     if usuario_completo[0] == usuario_BD and usuario_completo[1] == senha_BD:
       view.exibir_mensagem("pode entrar")
+      opcoes_menu()
     
     else:
         view.exibir_mensagem("usuário ou senha inválido")
@@ -15,3 +16,13 @@ def validar_login(usuario_completo):
 def iniciar():
     usuario_completo = view.formulario_login()
     validar_login(usuario_completo)
+
+def opcoes_menu():
+    opcao = view.menu()
+    if opcao == "1":
+        print("Cadastro de clientes")
+    elif opcao == "2":
+        print("listagem de clientes")
+    else:
+        view.exibir_mensagem("Sistema finalizado")
+        exit()
